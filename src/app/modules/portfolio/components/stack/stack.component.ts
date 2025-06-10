4748e3f55cd19f45aeb54bbaebb7117d8590a5a5
elziorocha <rochasenzo.04@gmail.com>
2025-06-10 14:46:03 -0300

    feat: inserção de ícones para stack e ajuste no componente de stack

--- src/app/modules/portfolio/components/stack/stack.component.ts
+++ src/app/modules/portfolio/components/stack/stack.component.ts
@@ -16,11 +16,35 @@ export class StackComponent {
     },
     {
       src: '/icons/css.svg',
-      color: 'hover:bg-blue-500'
+      color: 'hover:bg-blue-600'
     },
     {
       src: '/icons/tailwind.svg',
-      color: 'hover:bg-cyan-600'
+      color: 'hover:bg-teal-600'
+    },
+    {
+      src: '/icons/javascript.svg',
+      color: 'hover:bg-yellow-600'
+    },
+    {
+      src: '/icons/typescript.svg',
+      color: 'hover:bg-sky-800'
+    },
+    {
+      src: '/icons/react.svg',
+      color: 'hover:bg-blue-800'
+    },
+    {
+      src: '/icons/angular.svg',
+      color: 'hover:bg-rose-700'
+    },
+    {
+      src: '/icons/vue.svg',
+      color: 'hover:bg-emerald-600'
+    },
+    {
+      src: '/icons/npm.svg',
+      color: 'hover:bg-red-600'
     },
   ])
 }
