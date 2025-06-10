ed70d9315cc67d3eb7169f62da8ffe5930d8bc9d
elziorocha <rochasenzo.04@gmail.com>
2025-06-10 15:06:40 -0300

    feat: stack finalizado

--- src/app/modules/portfolio/components/stack/stack.component.ts
+++ src/app/modules/portfolio/components/stack/stack.component.ts
@@ -43,8 +43,68 @@ export class StackComponent {
       color: 'hover:bg-emerald-600'
     },
     {
-      src: '/icons/npm.svg',
-      color: 'hover:bg-red-600'
+      src: '/icons/cypress.svg',
+      color: 'hover:bg-green-400'
+    },
+    {
+      src: '/icons/docker.svg',
+      color: 'hover:bg-teal-700'
+    },
+    {
+      src: '/icons/ubuntu.svg',
+      color: 'hover:bg-amber-700'
+    },
+    {
+      src: '/icons/git.svg',
+      color: 'hover:bg-red-500'
+    },
+    {
+      src: '/icons/github.svg',
+      color: 'hover:bg-purple-600 pl-1.5'
+    },
+    {
+      src: '/icons/database.svg',
+      color: 'hover:bg-gray-400'
+    },
+    {
+      src: '/icons/sql.svg',
+      color: 'hover:bg-cyan-400'
+    },
+    {
+      src: '/icons/mongodb.svg',
+      color: 'hover:bg-emerald-800'
+    },
+    {
+      src: '/icons/supabase.svg',
+      color: 'hover:bg-green-700'
+    },
+    {
+      src: '/icons/prisma.svg',
+      color: 'hover:bg-zinc-300'
+    },
+    {
+      src: '/icons/node.svg',
+      color: 'hover:bg-emerald-700'
+    },
+    {
+      src: '/icons/php.svg',
+      color: 'hover:bg-violet-500'
+    },
+    {
+      src: '/icons/laravel.svg',
+      color: 'hover:bg-red-700'
+    },
+    {
+      src: '/icons/python.svg',
+      color: 'hover:bg-sky-600'
+    },
+    {
+      src: '/icons/nextjs.svg',
+      color: 'hover:bg-zinc-300'
+    },
+    {
+      src: '/icons/flutter.svg',
+      color: 'hover:bg-sky-500'
     },
   ])
 }
