7211d42270417a0afa8653fdac5b26c37c4cef46
elziorocha <rochasenzo.04@gmail.com>
2025-06-11 10:13:36 -0300

    feat: adição de seção de prpjetos com layout base

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,11 +1,93 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { ChevronRight, LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [CommonModule, LucideAngularModule],
   templateUrl: './projects.component.html',
 })
 
 export class ProjectsComponent {
+  readonly chevronRightIcon = ChevronRight;
 
+  public projects = signal([
+    {
+      title: 'PampaApp',
+      src: '/images/stars.png',
+      excerpt: `PampaApp é uma aplicação clone do website 'Twitter' ou 'X', contendo funcionalidades de publicação
+      de posts, blablabla`,
+      stack: [
+        '/icons/html.svg',
+        '/icons/css.svg',
+        '/icons/tailwind.svg',
+        '/icons/react.svg',
+        '/icons/prisma.svg',
+        '/icons/supabase.svg',
+      ],
+      link: 'https://pampaapp.vercel.app/'
+    },
+    {
+      title: 'PampaApp',
+      src: '/images/stars.png',
+      excerpt: `PampaApp é uma aplicação clone do website 'Twitter' ou 'X', contendo funcionalidades de publicação
+      de posts, blablabla`,
+      stack: [
+        '/icons/html.svg',
+        '/icons/css.svg',
+        '/icons/tailwind.svg',
+        '/icons/react.svg',
+        '/icons/prisma.svg',
+        '/icons/supabase.svg',
+      ],
+      link: 'https://pampaapp.vercel.app/'
+    },
+    {
+      title: 'PampaApp',
+      src: '/images/stars.png',
+      excerpt: `PampaApp é uma aplicação clone do website 'Twitter' ou 'X', contendo funcionalidades de publicação
+      de posts, blablabla`,
+      stack: [
+        '/icons/html.svg',
+        '/icons/css.svg',
+        '/icons/tailwind.svg',
+        '/icons/react.svg',
+        '/icons/prisma.svg',
+        '/icons/supabase.svg',
+      ],
+      link: 'https://pampaapp.vercel.app/'
+    },
+    {
+      title: 'PampaApp',
+      src: '/images/stars.png',
+      excerpt: `PampaApp é uma aplicação clone do website 'Twitter' ou 'X', contendo funcionalidades de publicação
+      de posts, blablabla`,
+      stack: [
+        '/icons/html.svg',
+        '/icons/css.svg',
+        '/icons/tailwind.svg',
+        '/icons/react.svg',
+        '/icons/prisma.svg',
+        '/icons/supabase.svg',
+      ],
+      link: 'https://pampaapp.vercel.app/'
+    },
+    {
+      title: 'PampaApp',
+      src: '/images/stars.png',
+      excerpt: `PampaApp é uma aplicação clone do website 'Twitter' ou 'X', contendo funcionalidades de publicação
+      de posts, blablabla`,
+      stack: [
+        '/icons/html.svg',
+        '/icons/css.svg',
+        '/icons/tailwind.svg',
+        '/icons/react.svg',
+        '/icons/prisma.svg',
+        '/icons/supabase.svg',
+        '/icons/vue.svg',
+        '/icons/angular.svg',
+      ],
+      link: 'https://pampaapp.vercel.app/'
+    },
+  ]);
 }
